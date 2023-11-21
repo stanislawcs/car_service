ab8e6a0ee8064872da9e67063e50791b61a08fb9
Stanislaw Shukan <shukans588@gmail.com>
2023-11-21 12:52:47 +0300

    Add liquibase, configure db, use docker-container, add changesets and changelogs

---
+++ src/main/resources/db/changelog/changeset/demo_data.sql
@@ -0,0 +1,9 @@
+insert into cars(brand,number,color,price) values('VOLKSWAGEN','1234KP-7','GREEN',45032);
+insert into cars(brand,number,color,price) values('HYUNDAI','0046KP-4','BLACK',50000);
+insert into cars(brand,number,color,price) values('VOLVO','2222KH-7','BLUE',60000);
+insert into cars(brand,number,color,price) values('MERCEDES','1111KP-5','WHITE',70000);
+
+
+insert into tech_inspection(car_id,date,address,services) values(1,03/04/2020,'Hrodna, Solomovaya, 33', '1-service, 2-service, 3-service');
+insert into tech_inspection(car_id,date,address,services) values(2,06/10/2022,'Hrodna, Folush, 11', '1-service, 2-service, 3-service');
+insert into tech_inspection(car_id,date,address,services) values(1,17/06/2022,'Minsk, Yakuba Kolasa, 57', '1-service, 2-service, 3-service');
